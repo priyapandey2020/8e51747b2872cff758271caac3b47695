a8e1a2066d187f882493f6af5b571d9894b425b1
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-06-25 10:49:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,21 +9,22 @@ function preload() {
     catImg2=loadAnimation("images/catWalking1.png","images/catWalking2.png");
     catImg3= loadAnimation("images/catHappy.png");
     mouseImg1=loadAnimation("images/mouseGift.png");
-    mouseImg2= loadAnimation("images/mouseTease1.png","images/mouseTease1.png");
+    mouseImg2= loadAnimation("images/mouseTease1.png","images/mouseTease2.png");
     mouseImg3=loadAnimation("images/mouseHappy.png");
-
 }
 
 function setup(){
     canvas = createCanvas(1000,800);
 
-    cat = createSprite(870, 600);
-    cat.addAnimation("catSleeping", catImg1);
-    cat.scale = 0.2;
-
     mouse = createSprite(200, 600);
     mouse.addAnimation("mouseStanding", mouseImg1);
     mouse.scale = 0.15;
+    
+    cat = createSprite(900, 600);
+    cat.addAnimation("catSleeping", catImg1);
+    cat.scale = 0.25;
+
+    makeCatWalk();
 
 }
 
@@ -41,20 +42,20 @@ function draw() {
         mouse.addAnimation("mouseLastImage", mouseImg3);
         mouse.scale=0.15;
         mouse.changeAnimation("mouseLastImage");
-    } 
+    }
+
 
     drawSprites();
 }
 
 
-function keyPressed(){
+function makeCatWalk(){
 
-    if(keyCode === LEFT_ARROW){
         mouse.addAnimation("mouseTeasing", mouseImg2)
         mouse.changeAnimation("mouseTeasing");
-        mouse.frameDelay = 25
+        mouse.frameDelay = 25; 
+
         cat.velocityX = -5; 
         cat.addAnimation("catRunning", catImg2);
         cat.changeAnimation("catRunning");
-    }
 }
